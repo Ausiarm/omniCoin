1f47a7e8d11c6fce95a20b179d83deb1df2314bb
Armstrong Ashton-Lewis <Ausiarm@gmail.com>
2021-10-26 08:15:57 +1000

    filled guides with real information and provided links to websites within guides.

--- client/src/utils/guideData.js
+++ client/src/utils/guideData.js
@@ -1,47 +1,53 @@
 import bitCoin from '../images/guideImages/bitcoinImg.png'
 
+// image: e.image,
+// link: e.link,
+// title: e.title,
+// summary: e.summary,
+// technologies: e.technologies
+
 const guideData = [
     {
         image: bitCoin,
-        link: 'https://themelioo.herokuapp.com/',
-        title: 'Themelio',
-        summary: 'An online platform for users to subscribe to health and wellness thought leaders who are looking to change the fitness landscape',
-        technologies: 'Ruby | Ruby on Rails | PostgreSQL | CSS | HTML 5 | Heroku | Git | Stripe'
+        link: 'https://www.investopedia.com/terms/c/cryptocurrency.asp',
+        title: 'What Is Cryptocurrency?',
+        summary: 'a form of payment that can be exchanged online for goods and services. Many companies have issued their own currencies, often called tokens, and these can be traded specifically for the good or service that the company provides. Think of them as you would arcade tokens or casino chips. You’ll need to exchange real currency for the cryptocurrency to access the good or service. online platform for users to subscribe to health and wellness thought leaders who are looking to change the fitness landscape',
+        Keywords: 'Satoshi Nakamoto | Bitcoin | Mining | Blockchain'
     },
     {
         image: bitCoin,
-        link: 'https://hackamons.netlify.app/',
-        title: 'Hackamon',
-        summary: 'An app to view digital versions of the original 151 pokemon cards',
-        technologies: 'Javascript | HTML 5 | CSS'
+        link: 'https://www.investopedia.com/terms/c/cryptocurrency.asp',
+        title: 'Why Should I Care About Crypto?',
+        summary: 'Many industry professionals believe that blockchain related technologies will disrupt many of the worlds largest industries, including finance and law. These technologies continue, every day, to change how industries interact and it is more likely than not that at some point in the future they will touch your own.',
+        Keywords: 'E-contracts | Encrypted | Decentralized'
     },
     {
         image: bitCoin,
-        link: 'https://armstrongal.netlify.app/',
-        title: '1st Portfolio',
-        summary: 'My first ever application!',
-        technologies: 'HTML | CSS'
+        link: 'https://www.canstar.com.au/investor-hub/buying-cryptocurrency/',
+        title: 'How Do I Purchase Crytpocurrency?',
+        summary: 'To buy cryptocurrencies, you will need to set up what is called a wallet. This is nothing more than an online application that can hold your currency. Then, in order to purchase you will need to create an account on an exchange and from there you transfer traditional currency into that account and begin to purchase what you are looking for. A good example of one of these exchanges is Coinbase. It combines the wallet and exchange concepts into a one stop shop for all things crypto',
+        Keywords: ' Crypto Wallets | Crypto Exchange | Coinbase'
     },
     {
         image: bitCoin,
-        link: 'https://armstrongal.netlify.app/',
-        title: '1st Portfolio',
-        summary: 'My first ever application!',
-        technologies: 'HTML | CSS'
+        link: 'https://cisomag.eccouncil.org/cryptocurrency-wallet-security/',
+        title: 'How To Protect My Crypto',
+        summary: 'There are a few options here. You can go with a cold wallet that is similar to a basic online wallet but does not connect to the internet. As a result they are not accessible to cyber attacks. Another option is making sure to always be on a secure internet connection when accessing your wallets or exchanges. Public wifi is a big no no here. ',
+        Keywords: 'Crypto Key | Cold Wallet | Hot Wallet | Phishing'
     },
     {
         image: bitCoin,
-        link: 'https://armstrongal.netlify.app/',
-        title: '1st Portfolio',
-        summary: 'My first ever application!',
-        technologies: 'HTML | CSS'
+        link: 'https://www.cbinsights.com/research/industries-disrupted-blockchain/',
+        title: 'I Bought Crypto, What Now?',
+        summary: 'First things first, start becoming literate in the Cryptocurrency sphere. You’ve started the journey into an exciting and vibrant ecosystem that evolves and changes everyday. Treat this as a hobby that you can devote small amounts of time to and in no time at all, this fancy crypto jargon will seem like a second language to you. There are more references below for anyone willing to dig deeper into the crypto world!',
+        Keywords: 'Global Investing | Economist | Tech-Disruptor'
     },
     {
         image: bitCoin,
-        link: 'https://armstrongal.netlify.app/',
-        title: '1st Portfolio',
-        summary: 'My first ever application!',
-        technologies: 'HTML | CSS'
+        link: 'https://theconversation.com/bitcoin-is-now-legal-tender-in-el-salvador-heres-what-that-means-167099',
+        title: 'How Can I Use My Crypto?',
+        summary: 'This is ultimately a personal decision, however, there are some trends that are gaining traction these days. For example, it can be used as a store of value. This idea is gaining traction as more and more people start to add cryptocurriencies to their portfolios. While they fluctuate a lot more than traditional stocks or bonds, they are an interesting way to spice up your portfolio and diversify into multiple industries.',
+        Keywords: 'Fiat Currency | Technological Deflation | Store of Value'
     },
 ]
 
